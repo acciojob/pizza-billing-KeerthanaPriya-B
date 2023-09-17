003ee4a52fe2e1a96dd69aa07772308b6fae0ce6
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2023-09-17 12:16:34 +0530

    made changes

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,14 +5,30 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-    private int total;
-    private int ExtraCheese = 0, ExtraToppings = 0, paperBag = 0;
-    Scanner sc = new Scanner(System.in);
+    private boolean cheeseadded;
+    private boolean toppoingsadded;
+
+    private boolean bagadded;
+    private boolean isbillgenerator;
+    private int cheeseprice;
+    private int toppingsprice;
+    private int bagprice;
+    private int tot;
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         //your code goes here --------------------------------
-        if(isVeg) {this.price = 300;}
-        else {this.price = 400;}
+        if(isVeg) {
+            this.price = 300;
+            toppingsprice = 70;
+        }
+        else {
+            this.price = 400;
+            toppingsprice = 120;
+        }
+        cheeseprice = 80;
+        bagprice = 20;
+        bill += "Base Price Of The Pizza: " + this.price+"\n";
     }
 
     public int getPrice(){
@@ -20,48 +36,45 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here --------------------------------
-        System.out.print("Want Extra Cheese [YES/NO]: ");
-        String ans = sc.nextLine();
-        if(ans.equals("YES") || ans.equals("yes")) {
-            ExtraCheese += 80;
+        // your code goes here -------------------------------
+        if(!cheeseadded) {
+            cheeseadded = true;
+            price += cheeseprice;
         }
-
     }
 
     public void addExtraToppings(){
         // your code goes here
-        System.out.print("Want Extra Toppings [YES/NO]: ");
-        String ans = sc.nextLine();
-        if(ans.equals("YES") || ans.equals("yes")) {
-            if(isVeg) ExtraToppings += 70;
-            else ExtraToppings += 120;
+        if(!toppoingsadded) {
+            toppoingsadded = true;
+            price += toppingsprice;
         }
     }
 
     public void addTakeaway(){
         // your code goes here
-        System.out.print("Want Paper Bag to Takeaway [YES/ NO]:");
-        String ans = sc.nextLine();
-        if(ans.equals("YES") || ans.equals("yes")) {
-            paperBag = 20;
+        if(!bagadded) {
+            bagadded = true;
+            price += bagprice;
         }
     }
 
     public String getBill(){
         // your code goes here
-        bill = "Base Price Of The Pizza: " + price;
-        if(ExtraCheese != 0) {
-            bill += "/nExtra Cheese Added: " + ExtraCheese;
+
+        if(!isbillgenerator) {
+            if(cheeseadded) {
+                bill += "Extra Cheese Added: " + cheeseprice +"\n";
+            }
+            if(toppoingsadded) {
+                bill += "Extra Toppings Added: " + toppingsprice+"\n";
+            }
+            if(bagadded) {
+                bill += "Paperbag Added: " + bagprice+"\n";
+            }
+            bill += "Total Price: " + price;
+            isbillgenerator = true;
         }
-        if(ExtraToppings != 0) {
-            bill += "/nExtra Toppings Added: " + ExtraToppings;
-        }
-        if(paperBag != 0) {
-            bill += "/nPaperbag Added: " + paperBag;
-        }
-        total += price+ExtraCheese+ExtraToppings+paperBag;
-        bill += "/nTotal Price: " + total;
         return this.bill;
     }
 }
